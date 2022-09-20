ea723b863ddde853316ad28d621a981743f38a77
Melvin Cotto <mcottocotto@knights.ucf.edu>
2022-09-20 15:13:35 -0400

    Added 2 new upgrades and readjusted player size

--- Assets/Scripts/PistolUpgrades.cs
+++ Assets/Scripts/PistolUpgrades.cs
@@ -3,8 +3,8 @@ using UnityEngine.UI;
 
 public class PistolUpgrades : MonoBehaviour, IDataPersistence
 {
-    [SerializeField] Button pistolDmgButton, pistolRofButton;
-    private bool isPistolDmgPurchasable, isPistolRoFPurchasable;
+    [SerializeField] Button pistolDmgButton, pistolRofButton, pistolAmmoButton, pistolReloadButton;
+    private bool isPistolDmgPurchasable, isPistolRoFPurchasable, isPistolAmmoPurchasable, isPistolReloadPurchasable;
 
     private void Awake()
     {
@@ -18,7 +18,7 @@ public class PistolUpgrades : MonoBehaviour, IDataPersistence
             pistolDmgButton.GetComponent<Image>().color = Color.red;
         }
 
-        if(isPistolRoFPurchasable && GameDataHolder.money >= 10000)
+        if(isPistolRoFPurchasable && GameDataHolder.money >= 1000)
         {
             pistolRofButton.GetComponent<Image>().color = Color.green;
         }
@@ -27,16 +27,40 @@ public class PistolUpgrades : MonoBehaviour, IDataPersistence
             pistolRofButton.interactable = false;
             pistolRofButton.GetComponent<Image>().color = Color.red;
         }
+
+        if(isPistolAmmoPurchasable && GameDataHolder.money >= 100)
+        {
+            pistolAmmoButton.GetComponent<Image>().color = Color.green;
+        }
+        else
+        {
+            pistolAmmoButton.interactable = false;
+            pistolAmmoButton.GetComponent<Image>().color = Color.red;
+        }
+        
+        if(isPistolReloadPurchasable && GameDataHolder.money >= 100)
+        {
+            pistolReloadButton.GetComponent<Image>().color = Color.green;
+        }
+        else
+        {
+            pistolReloadButton.interactable = false;
+            pistolReloadButton.GetComponent<Image>().color = Color.red;
+        }
     }
     public void LoadData(GameData data)
     {
         isPistolDmgPurchasable  = data.isPistolDmgPurchasable;
         isPistolRoFPurchasable = data.isPistolRoFPurchasable;
+        isPistolAmmoPurchasable = data.isPistolAmmoPurchasable;
+        isPistolReloadPurchasable = data.isPistolReloadPurchasable;
     }
     public void SaveData(GameData data)
     {
         data.isPistolDmgPurchasable = isPistolDmgPurchasable;
         data.isPistolRoFPurchasable = isPistolRoFPurchasable;
+        data.isPistolAmmoPurchasable = isPistolAmmoPurchasable;
+        data.isPistolReloadPurchasable = isPistolReloadPurchasable;
     }
 
     public void PurchasePistolDamage()
@@ -56,15 +80,43 @@ public class PistolUpgrades : MonoBehaviour, IDataPersistence
 
     public void PurchasePistolRoF()
     {
-        if (isPistolRoFPurchasable && GameDataHolder.money >= 10000)
+        if (isPistolRoFPurchasable && GameDataHolder.money >= 1000)
         {
             isPistolRoFPurchasable = false;
             pistolRofButton.GetComponent<Image>().color = Color.red;
             pistolRofButton.interactable = false;
-            GameDataHolder.money -= 10000;
+            GameDataHolder.money -= 1000;
             MoneyHolderUI.instance.moneyUI.text = GameDataHolder.money.ToString();
             GameDataHolder.pistolFireRate += 2;
             Debug.Log("Pistol RoF is now equal to " + GameDataHolder.pistolFireRate);
+        }
+    }
+
+    public void PurchasePistolAmmo()
+    {
+        if (isPistolAmmoPurchasable && GameDataHolder.money >= 1000)
+        {
+            isPistolAmmoPurchasable = false;
+            pistolAmmoButton.GetComponent<Image>().color = Color.red;
+            pistolAmmoButton.interactable = false;
+            GameDataHolder.money -= 1000;
+            MoneyHolderUI.instance.moneyUI.text = GameDataHolder.money.ToString();
+            GameDataHolder.pistolMagazine += 4;
+            Debug.Log("Pistol RoF is now equal to " + GameDataHolder.pistolFireRate);
+        }
+    }
+
+    public void PurchasePistolReload()
+    {
+        if(isPistolReloadPurchasable && GameDataHolder.money >= 1000)
+        {
+            isPistolReloadPurchasable = false;
+            pistolReloadButton.GetComponent<Image>().color = Color.red;
+            pistolReloadButton.interactable = false;
+            GameDataHolder.money -= 1000;
+            MoneyHolderUI.instance.moneyUI.text = GameDataHolder.money.ToString();
+            GameDataHolder.pistolReloadTime -= 1.0f;
+            Debug.Log("Pistol Reload Time is now " + GameDataHolder.pistolReloadTime);
         }
     }
 }
